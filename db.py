0b0c72c05a4ac71f882efab69bf239b7d05a852e
naka520 <yashipipi0520@gmail.com>
2023-10-01 00:09:08 +0900

    [modify]第3回分追加

--- db.py
+++ db.py
@@ -1,17 +1,3 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
--- urls.py
+++ urls.py
@@ -4,3 +4,4 @@ from controllers import *
 # FastAPIのルーティング用関数
 app.add_api_route('/', index)
 app.add_api_route('/admin', admin)  # new
+app.add_api_route('/register', register, methods=['GET', 'POST'])  # new
